06ecb684b9b95de15abba82ffee3e5f0f21b6d9c
Đôn Kiều <kieuhuydon@gmail.com>
2026-04-22 09:45:49 +0700

    feat: UI tour management

--- frontend/src/routes/AdminRoutes.jsx
+++ frontend/src/routes/AdminRoutes.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import AdminLayout from '../layouts/AdminLayout/AdminLayout';
 import Dashboard from '../pages/admin/Dashboard';
+import CreateTour from '../pages/admin/Tours/CreateTour';
+import UpdateTour from '../pages/admin/Tours/UpdateTour';
 import UserManagement from '../pages/admin/Users/UserManagement';
+import TourManagement from '../pages/admin/Tours/TourManagement';
 
 const AdminRoutes = () => {
   return (
@@ -10,6 +13,9 @@ const AdminRoutes = () => {
       <Route element={<AdminLayout />}>
         <Route path="dashboard" element={<Dashboard />} />
         <Route path="users" element={<UserManagement />} />
+        <Route path="tours" element={<TourManagement />} />
+        <Route path="tours/create" element={<CreateTour />} />
+        <Route path="tours/edit/:id" element={<UpdateTour />} />
         {/* If hitting /admin, redirect to /admin/dashboard */}
         <Route path="/" element={<Navigate to="dashboard" replace />} />
       </Route>
