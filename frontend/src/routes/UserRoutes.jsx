2133db45e9d4d5dd19a609a0134defc479b7077f
Đôn Kiều <kieuhuydon@gmail.com>
2026-05-02 01:04:45 +0700

    Fix UserLayout import to use the new frontend-shared layout

--- frontend/src/routes/UserRoutes.jsx
+++ frontend/src/routes/UserRoutes.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import UserLayout from '../layouts/UserLayout/UserLayout';
+import UserLayout from '../layouts/UserLayout';
 import Home from '../pages/user/Home';
 
 const UserRoutes = () => {
