472b3e09f5a067ddfc573c79afa6eea640f458d1
unknown <nguyentrongbaoviet@gmail.com>
2026-05-03 08:32:53 +0700

    fix error when login basic accout

--- backend/demo/src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
+++ backend/demo/src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.example.demo.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -41,6 +42,14 @@ public class GlobalExceptionHandler {
         errorDetails.put("errors", errors);
         
         return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<?> handleAuthenticationException(AuthenticationException ex) {
+        Map<String, Object> errorDetails = new HashMap<>();
+        errorDetails.put("timestamp", new Date());
+        errorDetails.put("message", ex.getMessage());
+        return new ResponseEntity<>(errorDetails, HttpStatus.UNAUTHORIZED);
     }
 
     @ExceptionHandler(Exception.class)
